48b4c39efc410f2ac13f5b2d019a94bf06bb19e3
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2018-07-23 13:16:31 +0200

    gradle updating

---
+++ app/src/main/java/com/adja/apps/mohamednagy/androidarch/database/Note.java
@@ -0,0 +1,9 @@
+package com.adja.apps.mohamednagy.androidarch.database;
+
+/**
+ * Created by Mohamed Nagy on 7/23/2018 .
+ * Project android_project
+ * Time    12:51 PM
+ */
+public class Note {
+}
