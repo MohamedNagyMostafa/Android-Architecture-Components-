d76894494954afe39bff5f4df5a4e34e8579f2b9
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2018-07-23 20:41:00 +0200

    List Screen Implementation

--- app/src/main/java/com/adja/apps/mohamednagy/androidarch/MainActivity.java
+++ app/src/main/java/com/adja/apps/mohamednagy/androidarch/MainActivity.java
@@ -1,13 +1,57 @@
 package com.adja.apps.mohamednagy.androidarch;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import com.adja.apps.mohamednagy.androidarch.database.AppDatabase;
+import com.adja.apps.mohamednagy.androidarch.database.Note;
+import com.adja.apps.mohamednagy.androidarch.ui.AddNoteActivity;
+import com.adja.apps.mohamednagy.androidarch.ui.NoteAdapter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppDatabase mAppDatabase;
+    private NoteAdapter mNoteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppDatabase = AppDatabase.getInstance(getApplicationContext());
+        mNoteAdapter = new NoteAdapter(this);
+
+        ListView listView = findViewById(R.id.note_list);
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_new_note_btn);
+
+        listView.setAdapter(mNoteAdapter);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddNoteActivity.class));
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Note> notes = mAppDatabase.noteDao().loadAllNotes();
+        mNoteAdapter.swapList(notes);
+    }
+
 }
---
+++ app/src/main/java/com/adja/apps/mohamednagy/androidarch/ui/NoteAdapter.java
@@ -0,0 +1,71 @@
+package com.adja.apps.mohamednagy.androidarch.ui;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.adja.apps.mohamednagy.androidarch.MainActivity;
+import com.adja.apps.mohamednagy.androidarch.R;
+import com.adja.apps.mohamednagy.androidarch.database.Note;
+import com.adja.apps.mohamednagy.androidarch.databinding.NoteItemBinding;
+
+import java.util.List;
+
+/**
+ * Created by Mohamed Nagy on 7/23/2018 .
+ * Project android_project
+ * Time    5:59 PM
+ */
+public class NoteAdapter extends ArrayAdapter<Note> {
+    private List<Note> mNotes;
+
+    public NoteAdapter(@NonNull Context context) {
+        super(context, 0);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Note note = mNotes.get(position);
+        NoteItemBinding noteItemBinding;
+
+        if(convertView == null)
+            noteItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                    R.layout.note_item, parent, false);
+        else
+            noteItemBinding = DataBindingUtil.bind(convertView);
+
+        assert noteItemBinding != null;
+
+        noteItemBinding.authorTextView.setText(note.getAuthor());
+        noteItemBinding.headTextView.setText(note.getHead());
+        noteItemBinding.bodyTextView.setText(note.getBody());
+        noteItemBinding.dateTextView.setText(Util.dateToString(note.getUpdatedAt()));
+        int pe = note.getPriority();
+        Log.e("int ttt test", "" +pe);
+        noteItemBinding.priorityValue.setText(String.valueOf(pe));
+        noteItemBinding.priorityFrame.setBackground(parent.getContext().getDrawable(Util.colorSwitcher(note.getPriority())));
+
+        return noteItemBinding.getRoot();
+    }
+
+    public void swapList(List<Note> notes){
+        mNotes = notes;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return mNotes == null?0: mNotes.size();
+    }
+
+}
